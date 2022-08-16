b34c21cd4fc7b24a5fcd0982a565a8fcb82de705
andrey.sh <aash7133@gmail.com>
2022-08-16 10:18:26 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+
+function clearActiveClasses() {
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+  });
+}
+
+slides.forEach((slide) => {
+  slide.addEventListener('click', () => {
+    clearActiveClasses();
+    slide.classList.add('active');
+  });
+});
